05d588261f6890fee7079013b40fda7b170a49d3
Atsushi Yausda <atsushi.yasuda.jp@gmail.com>
2023-02-05 18:07:21 +0900

    WIP

---
+++ python/string/initialize.py
@@ -0,0 +1,28 @@
+
+def debug(s):
+    print(s)
+
+debug('python')
+
+debug('Hello, World.')
+debug("Hello, World.")
+debug("""
+Hello, World.
+""")
+
+# 接頭辞
+
+
+# byte になる
+b = b'hogehoge' # B'hogehoge'
+
+# これなんだ？
+r = r'hogehoge'
+
+# f はフォーマット済み文字列リテラル　
+s = 'is it ok?'
+f = f'formatted {s}' # python 3.6 から
+
+# u = u'hogehoge'
+
+
